050063db83085958afa8b0d601aa5a29a78bc7e8
doto052687 <doto052687@gmail.com>
2019-03-14 22:20:06 +0800

    test

--- test/test/test.cpp
+++ test/test/test.cpp
@@ -3,20 +3,7 @@
 #include <iostream>
 using namespace System;
 bool Guess(int number) {
-	static int target = -1;
-	if (target == -1) {
-		Random r;
-		target = r.Next() % 100 + 1;
-	}
-	if (number > target) {
-		std::cout << "Smaller" << std::endl;
-		return false;
-	}
-	else if (number < target) {
-		std::cout << "Bigger" << std::endl;
-		return false;
-	}
-	return true;
+	return false;
 }
 int main(array<System::String ^> ^args)
 {
